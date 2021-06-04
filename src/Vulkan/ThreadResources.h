9b226a29c39aa48bb2291eaf024f3c9482aacd6c
Shane Hyde <shane@noctonyx.com>
2021-06-05 00:02:18 +1000

    Remove Descriptor Sets from Thread Resources

--- src/Vulkan/ThreadResources.h
+++ src/Vulkan/ThreadResources.h
@@ -2,52 +2,27 @@
 // Created by shane on 14/02/2021.
 //
 
-#ifndef RX_THREADRESOURCES_H
-#define RX_THREADRESOURCES_H
+#pragma once
 
 #include <memory>
 #include "Vulkan/Vulk.hpp"
-//#include "Vulkan/Device.h"
 #include "Vulkan/DescriptorPool.hpp"
 #include "Pooler.h"
-#include "Hasher.h"
 
 namespace RxCore
 {
     class SecondaryCommandBuffer;
-    class DescriptorSet;
-    //class DescriptorPool;
     class CommandPool;
-    //struct DescriptorPoolGroup;
-    //struct DescriptorPoolTemplate;
 
     struct ThreadResources
     {
         std::shared_ptr<SecondaryCommandBuffer> getCommandBuffer();
-
-        std::shared_ptr<DescriptorSet> getDescriptorSet(
-            const DescriptorPoolTemplate & poolTemplate,
-            vk::DescriptorSetLayout layout);
-
-        std::shared_ptr<DescriptorSet> getDescriptorSet(
-            const DescriptorPoolTemplate & poolTemplate,
-            vk::DescriptorSetLayout layout,
-            const std::vector<uint32_t> & counts);
-
-        //std::shared_ptr<RxUtil::Pooler<RxCore::DescriptorSet>> getSetPoooler(uint32_t id);
-        //void setSetPooler(uint32_t id, std::shared_ptr<RxUtil::Pooler<RxCore::DescriptorSet>> setPooler);
-
-        //void registerDescriptorPool(uint32_t id, const std::vector<vk::DescriptorPoolSize> & poolSizes, uint32_t max);
-        //void unregisterDescriptorPool(uint32_t id);
 
         void freeAllResources();
         void freeUnused();
 
         std::shared_ptr<CommandPool> pool;
         std::deque<std::shared_ptr<SecondaryCommandBuffer>> buffers;
-        //std::unordered_map<uint32_t, std::shared_ptr<RXUtil::Pooler<RXCore::DescriptorSet>>> setPoolers;
-        //std::unordered_map<uint32_t, std::shared_ptr<DescriptorPool>> descriptorSetPools;
-        std::unordered_map<RxUtil::Hash, DescriptorPoolGroup> descriptorSetPoolGroups;
         std::string threadId;
     };
 
@@ -63,4 +38,3 @@ namespace RxCore
         threadResources.freeAllResources();
     }
 }
-#endif //RX_THREADRESOURCES_H
